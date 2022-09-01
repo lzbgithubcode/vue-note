ff2d87b97911f8c25970f52de166550052ce4727
lzb <liuzibin@timingbar.com>
2022-09-01 18:21:04 +0800

    next-tick

--- src/core/util/next-tick.js
+++ src/core/util/next-tick.js
@@ -7,13 +7,19 @@ import { isIE, isIOS, isNative } from './env'
 
 export let isUsingMicroTask = false
 
+// 定义回调队列
 const callbacks = []
+// 定义异步锁
 let pending = false
 
+// 执行队列中的每一个回调
 function flushCallbacks () {
+    // 重置异步锁
   pending = false
+  // // 防止出现nextTick中包含nextTick时出现问题, 所以复制一份回到会清空回到
   const copies = callbacks.slice(0)
   callbacks.length = 0
+  // 遍历执行回调
   for (let i = 0; i < copies.length; i++) {
     copies[i]()
   }
@@ -44,18 +50,15 @@ let timerFunc
  * 是否支持micro tasks ： Promise -> MutationObserver -> setImmediate
  * 都不支持使用macro tasks : setTimeout
  */
+// 1. 检测是否支持原生 setImmediate(高版本 IE 和 Edge 支持)
 if (typeof Promise !== 'undefined' && isNative(Promise)) {
   const p = Promise.resolve()
   timerFunc = () => {
     p.then(flushCallbacks)
-    // In problematic UIWebViews, Promise.then doesn't completely break, but
-    // it can get stuck in a weird state where callbacks are pushed into the
-    // microtask queue but the queue isn't being flushed, until the browser
-    // needs to do some other work, e.g. handle a timer. Therefore we can
-    // "force" the microtask queue to be flushed by adding an empty timer.
     if (isIOS) setTimeout(noop)
   }
   isUsingMicroTask = true
+// 2. 检测是否支持原生MutationObserver
 } else if (!isIE && typeof MutationObserver !== 'undefined' && (
   isNative(MutationObserver) ||
   // PhantomJS and iOS 7.x
@@ -75,6 +78,7 @@ if (typeof Promise !== 'undefined' && isNative(Promise)) {
     textNode.data = String(counter)
   }
   isUsingMicroTask = true
+// 3. 检测是否支持原生setImmediate(是宏任务，但是比setTimeout好)
 } else if (typeof setImmediate !== 'undefined' && isNative(setImmediate)) {
   // Fallback to setImmediate.
   // Technically it leverages the (macro) task queue,
@@ -84,6 +88,7 @@ if (typeof Promise !== 'undefined' && isNative(Promise)) {
   }
 } else {
   // Fallback to setTimeout.
+  // 4. 最后只能选择setTimeout
   timerFunc = () => {
     setTimeout(flushCallbacks, 0)
   }
@@ -91,6 +96,7 @@ if (typeof Promise !== 'undefined' && isNative(Promise)) {
 
 export function nextTick (cb?: Function, ctx?: Object) {
   let _resolve
+  // 将回调函数增加到回调队列中
   callbacks.push(() => {
     if (cb) {
       try {
@@ -102,10 +108,13 @@ export function nextTick (cb?: Function, ctx?: Object) {
       _resolve(ctx)
     }
   })
+  // 如果没有锁，那就锁上异步锁，准备等同步函数执行完后，就开始执行回调函数队列
+  // 接收第一个回调函数时，先关上锁，执行异步方法
   if (!pending) {
     pending = true
     timerFunc()
   }
+  //     // 如果没有提供回调，并且支持Promise，返回一个Promise
   // $flow-disable-line
   if (!cb && typeof Promise !== 'undefined') {
     return new Promise(resolve => {
